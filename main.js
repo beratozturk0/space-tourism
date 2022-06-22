99606c808eaa712872d9fa2e311ad310cb44479d
Berat <brtozturk01@gmail.com>
2022-06-22 21:18:39 +0300

    new page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './styles/style.scss'
 import './styles/destination.scss'
 import './styles/crew.scss'
+import './styles/technology.scss'
 const qs = (selector, parent = document) => {
   return parent.querySelector(selector)
 }
@@ -57,8 +58,6 @@ buttons.forEach((b) => {
     })
   }
 })
-
-
 const crew = [{
     id: 1,
     img: "/assets/crew/image-douglas-hurley.png",
@@ -106,4 +105,32 @@ dots.forEach((d) => {
   }
 
 })
-console.log(crew[0])
+const technology = [{
+    id: 1,
+    img: "/assets/technology/image-launch-vehicle-portrait.jpg",
+    title: "LAUNCH VEHICLE",
+    text: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    id: 2,
+    img: "/assets/technology/image-spaceport-portrait.jpg",
+    title: "SPACEPORT",
+    text: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+  },
+  {
+    id: 3,
+    img: "/assets/technology/image-space-capsule-portrait.jpg",
+    title: "SPACE CAPSULE",
+    text: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
+]
+qs(".technology .circles").onclick = (e) => {
+  if (e.target.className === "circle") {
+    // console.log(e.target.textContent - 1)
+    const index = e.target.textContent - 1
+    // console.log(technology[index].title)
+    qs(".title").textContent = technology[index].title
+    qs(".text").textContent = technology[index].text
+    qs(".imgWrapper img").src = technology[index].img
+  }
+}
